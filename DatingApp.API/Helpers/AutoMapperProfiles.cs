dd52b154a1f7b70c02e7ba9338732a4e55b47914
piotr sedlak <piotr.sedlak@outlook.com>
2018-10-04 21:21:28 +0200

    editing user profile back+front

--- DatingApp.API/Helpers/AutoMapperProfiles.cs
+++ DatingApp.API/Helpers/AutoMapperProfiles.cs
@@ -24,6 +24,7 @@ namespace DatingApp.API.Helpers
                 opt.ResolveUsing(d => d.DateOfBirth.CalculateAge());
             });
             CreateMap<Photo,PhotosForDetailsDTO>();
+            CreateMap<UserForUpdateDTO,User>();
         }
     }
 }
